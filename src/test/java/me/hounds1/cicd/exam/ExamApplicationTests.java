39d4bdac34e32f582b4dd06227c96ebd9599686e
Hounds1 <zoodisx@gmail.com>
2023-07-06 23:31:56 +0900

    git init

---
+++ src/test/java/me/hounds1/cicd/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hounds1.cicd.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
