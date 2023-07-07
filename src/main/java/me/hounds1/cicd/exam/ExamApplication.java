71a0ff77b32369e7ece1578a713b81e92214cc05
Hounds1 <zoodisx@gmail.com>
2023-07-07 14:29:38 +0900

    Ngrok + Jenkins webhook test

--- src/main/java/me/hounds1/cicd/exam/ExamApplication.java
+++ src/main/java/me/hounds1/cicd/exam/ExamApplication.java
@@ -2,12 +2,16 @@ package me.hounds1.cicd.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExamApplication {
+public class ExamApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExamApplication.class, args);
+		SpringApplication application = new SpringApplication(ExamApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
--- src/main/java/me/hounds1/cicd/exam/global/JenkinsRebuild.java
+++ src/main/java/me/hounds1/cicd/exam/global/JenkinsRebuild.java
@@ -3,5 +3,6 @@ package me.hounds1.cicd.exam.global;
 public class JenkinsRebuild {
 
     public void jenkinsRebuildTest() {
+        // TODO: 2023-07-07 ngrok으로 jenkins 포워딩 + webhook 연결
     }
 }
